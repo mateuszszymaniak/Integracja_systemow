98d75e73082c2dba7c455f414e8d33b972864ea1
Cougar Dev <m.szewczyk58@gmail.com>
2022-04-08 19:41:56 +0200

    Creation of a REST application structure

---
+++ REST/class/Projects.php
@@ -0,0 +1,14 @@
+<?php
+class Cities{
+    //TODO
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    function read(){
+        //TODO
+    }
+    function write(){
+        //TODO
+    }
+}
+?>
