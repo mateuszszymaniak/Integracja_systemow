a1b2efb9f03b32ab93599fe218a54fb237228035
Cougar Dev <m.szewczyk58@gmail.com>
2022-04-15 17:59:22 +0200

    Update Projects.php
    READ functionality working properly

--- REST/class/Projects.php
+++ REST/class/Projects.php
@@ -1,7 +1,7 @@
 <?php
 class Project{
     private $dbTable = "project";
-    public $idproject;
+    public $id;
     public $title;
     public $description;
     public $contract_no;
@@ -14,7 +14,15 @@ class Project{
         $this->conn = $db;
     }
     function read(){
-        //TODO
+        if($this->id) {
+            $stmt = $this->conn->prepare("SELECT * FROM ".$this->dbTable." WHERE idproject = ?");
+            $stmt->bind_param("i", $this->id);
+        } else {
+            $stmt = $this->conn->prepare("SELECT * FROM ".$this->dbTable);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result;
     }
     function write(){
         //TODO
