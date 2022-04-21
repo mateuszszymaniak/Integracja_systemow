104f3d4bd6cc04d8a73c44d64e60d0e298fb0b02
Cougar Dev <m.szewczyk58@gmail.com>
2022-04-21 21:17:47 +0200

    Update main.py

--- Client/main.py
+++ Client/main.py
@@ -1,9 +1,27 @@
+from types import SimpleNamespace
+
 import matplotlib
 import requests
+import json
 
 url = "http://localhost/Integracja_systemow/REST/main/read"
 response = requests.get(url)
-print(response)
 
-print()
-print(response.json())
+try:
+    response = requests.get(url)
+    json_struct = json.dumps(response.json(), indent=4)
+
+    if (response.status_code == 200):
+        array = json.loads(json_struct)
+
+        print(response.json())
+
+        print()
+        print(array['project'][0]['title'])
+        print(type(array['project'][0]['idproject']))
+
+    elif (response.status_code == 404):
+        print("Result not found!")
+
+except requests.ConnectionError as error:
+    print(error)
