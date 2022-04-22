4b593ff3451e459362ec7442e50ad810d2d63ade
Cougar Dev <m.szewczyk58@gmail.com>
2022-04-22 19:06:20 +0200

    Update main.py
    Console app is working

--- Client/console version/main.py
+++ Client/console version/main.py
@@ -3,35 +3,54 @@ import numpy as np
 import requests
 import json
 
-def woj(array, woj="LUBELSKIE"):
-    tab = np.array()
-    tab[0] = np.array()
-    tab[1] = np.array()
-    for project in array['project_location']:
+
+def conn(url):
+    try:
+        response = requests.get(url)
+        json_struct = json.dumps(response.json(), indent=4)
+
+        if (response.status_code == 200):
+            array = json.loads(json_struct)
+
+            # print(response.json())
+            return array
+
+        elif (response.status_code == 404):
+            print("Result not found!")
+
+    except requests.ConnectionError as error:
+        print(error)
+
+
+def woj(location, finance, duration, woj="LUBELSKIE"):
+    id = []
+    xAxis = []
+    yAxis = []
+
+    for indx, project in enumerate(location['project_location']):
         if woj in project['location']:
-            id = project['idproject_location']
+            id.append(location['project'][indx]['idproject'])
+    for indx, project in enumerate(finance['project']):
+        if project['idproject'] in id:
+            yAxis.append(finance['finance'][indx]['total_value'])
+            yAxis = np.array(yAxis)
+    for indx, project in enumerate(duration['project']):
+        if project['idproject'] in id:
+            xAxis.append(duration['duration'][indx]['start'][:4])
+            xAxis = np.array(xAxis)
+    return (xAxis, yAxis)
 
 
-url = "http://localhost/Integracja_systemow/REST/main/read"
-response = requests.get(url)
+url = "http://localhost/Integracja_systemow/REST/main/read/location"
+location = conn(url)
 
-try:
-    response = requests.get(url)
-    json_struct = json.dumps(response.json(), indent=4)
+url = "http://localhost/Integracja_systemow/REST/main/read/finance"
+finance = conn(url)
 
-    if (response.status_code == 200):
-        array = json.loads(json_struct)
+url = "http://localhost/Integracja_systemow/REST/main/read/duration"
+duration = conn(url)
+x, y = woj(location, finance, duration, "LUBELSKIE")
+# print(arr)
 
-        print(response.json())
-
-        print()
-        print(array['project'][0]['title'])
-        print(type(array['project'][0]['idproject']))
-
-        woj(array)
-
-    elif (response.status_code == 404):
-        print("Result not found!")
-
-except requests.ConnectionError as error:
-    print(error)
+plt.plot(x, y, 'bo')
+plt.show()
