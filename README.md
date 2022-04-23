247cc03f0a1df5e4ace9ee6e84db62c8323bb2c6
Mateusz Szymaniak <maszipl@gmail.com>
2022-04-23 15:22:08 +0200

    Revert last commit
    This reverts commit 11096361b12dfa0d58ab58160c0178c75a491a6f.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Integracja_systemow
+
+## Precondition:
+### 1. To generate XML file from xlsx file you need:
+###		1.1 Open Program.cs from XML_Converter folder.
+###		1.2 Press Project from toolbar and press Add Reference...
+###		1.3 Press on COM (from left site of window).
+###		1.4 On serach toolbox (top right side on window) write "excel".
+###		1.5 Mark checkbox for Microsoft Excel 16.0 Object Library.
+### 	1.6 Press OK.
+### 	1.7 Run the program.
---
+++ REST/class/Beneficiaries.php
@@ -0,0 +1,17 @@
+<?php
+class Beneficiary{
+    private $dbTable = "beneficiary";
+    public $id;
+    public $name;
+    public function __construct($db){
+        $this->conn = $db;
+    }
+    function read($projId){
+        $stmt = $this->conn->prepare("SELECT * FROM ".$this->dbTable." WHERE idbeneficiary = ?");
+        $stmt->bind_param("i", $this->id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result;
+    }
+}
+?>
