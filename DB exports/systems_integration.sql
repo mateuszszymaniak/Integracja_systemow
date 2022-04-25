ab16dcd9ca809fa5006ecddcdef26f49d772d21a
Cougar Dev <m.szewczyk58@gmail.com>
2022-04-25 19:29:35 +0200

    Improved sql script

--- DB exports/systems_integration.sql
+++ DB exports/systems_integration.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4936bf8fbb6727da3e2f051ea5684faf1eae117d7ece8a6455f8de4a16dea878
-size 108362657
+oid sha256:9e378a4178ef064f56c9fcabd44eb7e50a47ce63358b9672d520c695613d6ca5
+size 108362763
